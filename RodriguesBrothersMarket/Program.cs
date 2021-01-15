d3712f16193161b32d2b4a520e52cf397ff7ed13
João Rodrigues <a20629@alunos.ipca.pt>
2021-01-15 23:18:20 +0000

    Working on menu and Class User

--- RodriguesBrothersMarket/Program.cs
+++ RodriguesBrothersMarket/Program.cs
@@ -6,7 +6,43 @@ namespace RodriguesBrothersMarket
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            User one = new User(Position.manager, "João Rodrigues", "abc");
+            User two = new User(Position.manager, "Jorge Rodrigues", "def");
+
+            int mainOption = 0;
+                while (mainOption != 3)
+                {
+                    Console.WriteLine("**RodriguesBrothersMarket**");
+                    Console.WriteLine("1 - Login");
+                    Console.WriteLine("2 - Criar Funcionário");
+                    Console.WriteLine("3 - Sair");
+
+                mainOption = int.Parse(Console.ReadLine());
+                    Console.Clear();
+
+                    switch (mainOption)
+                    {
+                        case 1:
+                            Console.WriteLine("Testar Login");
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Testar criar Funcionário");
+                            break;
+
+                        case 3:
+                            Console.WriteLine("Testar Sair");
+                            break;
+
+                        default:
+                            Console.WriteLine("Escolha uma opção válida");
+                            break;
+                    }
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+                Console.WriteLine("Obrigado pela sua preferência");
         }
     }
 }
--- RodriguesBrothersMarket/User.cs
+++ RodriguesBrothersMarket/User.cs
@@ -4,7 +4,36 @@ using System.Text;
 
 namespace RodriguesBrothersMarket
 {
+    enum Position{
+        manager,
+        chashier,
+        replanisher
+    }
+    
     class User
     {
+        //Variables:
+        public Position position;
+        public string name;
+        public string password;
+
+        //Constructor
+        public User(Position position, string name, string password)
+        {
+            this.position = position;
+            this.name = name;
+            this.password = password;
+        }
+
+        //List:
+        public List<User> userList;
+
+        public User()
+        {
+            this.userList = new List<User>();
+        }
+
+        //Methods:
+        
     }
 }
