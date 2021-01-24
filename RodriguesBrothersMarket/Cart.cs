506f9010d702cee291a3dc6f602fa7e7f22ab613
Jorge Rodrigues <a20632@alunos.ipca.pt>
2021-01-24 19:50:57 +0000

    Creating Class Cart and Invoice
    Creating Class Cart and Invoice

---
+++ RodriguesBrothersMarket/Cart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RodriguesBrothersMarket
+{
+    class Cart
+    {
+        public List<Invoice> cartList;
+
+        public Cart()
+        {
+            this.cartList = new List<Invoice>();
+        }
+    }
+
+
+}
---
+++ RodriguesBrothersMarket/Invoice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+
+namespace RodriguesBrothersMarket
+{
+    class Invoice : Product
+    {
+        public int invoiceNumber;
+        public DateTime invoiceDate;
+        public string customerName;
+        //falta o nome do utilizador
+
+        public Invoice(int invoiceNumber, DateTime invoiceDate, string customerName, string productName, int productQnt, int price) : base(productName, productQnt, price)
+        {
+
+        }
+        
+    }
+}
