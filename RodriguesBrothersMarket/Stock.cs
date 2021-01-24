d3162521d42157275214c83f230da577189d9f06
Jorge Rodrigues <a20632@alunos.ipca.pt>
2021-01-24 18:06:05 +0000

    Create 3 methods in Stock Class
    Delete,Clean,AddS Stock

--- RodriguesBrothersMarket/Stock.cs
+++ RodriguesBrothersMarket/Stock.cs
@@ -12,5 +12,40 @@ namespace RodriguesBrothersMarket
         {
             this.productList = new List<Product>();
         }
+
+        //Metodos:
+
+        //Para criar novo produto e gravar na lista de produtos:
+        public Product CreateProduct(string productName, int productQnt, float price)
+        {
+            Product newProduct = new Product(productName, productQnt, price);
+            this.productList.Add(newProduct);
+            return newProduct;
+        }
+
+        //Método para Apagar um Produto:
+        public bool DeleteProductFromList(string productName)
+        {
+            int indexToDelete = -1;
+            for (int i = 0; i < productList.Count; i++)
+            {
+                if (this.productList[i].productName.ToLower().Equals(productName.ToLower()))
+                {
+                    indexToDelete = i;
+                    return true;
+                }
+            }
+            if (indexToDelete != -1)
+            {
+                this.productList.RemoveAt(indexToDelete);
+            }
+            return true;
+        }
+        // Método Limpar Stock:
+        public bool ClearStock()
+        {
+            this.productList.Clear();
+            return true;
+        }
     }
 }
